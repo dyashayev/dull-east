58bde9d8afd97982f0e27898572ed78e21f37e5d
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2017-09-13 11:47:04 -0400

    I copied the teacher

--- Dull-west/src/Main/Duel.java
+++ Dull-west/src/Main/Duel.java
@@ -1,5 +1,31 @@
 package Main;
 
+
+
+
 public class Duel {
+
+	
+
+	private Dueler d1;
+	private Dueler d2;
+	
+	
+	
+	
+	public static void main(String[] args){
+		Duel duel = new Duel();
+		duel.dukeItOut();
+	}
+	
+	public Duel() {
+		d1 = new CharacterA();
+		d2 = new CharacterB();
+	}
+	
+	public void dukeItOut(){
+		
+	}
+	
 
 }
