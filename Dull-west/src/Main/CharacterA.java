b3891892a367b60ce38972e90373309d98884831
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2017-09-14 11:46:57 -0400

    Nockles came in clutch

--- Dull-west/src/Main/CharacterA.java
+++ Dull-west/src/Main/CharacterA.java
@@ -1,5 +1,24 @@
 package Main;
 
-public class CharacterA {
-
+public class CharacterA implements Dueler {
+	
+	private int hp;
+	
+	public CharacterA() {
+		
+	}
+	
+	public String getName() {
+		return "The winner";
+	}
+	
+	public void taunt() {
+		System.out.println("You Best Skedaddle");
+	}
+	public void setStartingHP(int hp) {
+		this.hp = hp;
+	}
+	public int getHP() {
+		return hp;
+	}
 }
